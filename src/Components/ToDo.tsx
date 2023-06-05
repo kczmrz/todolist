e47338fc44a98eff89da6fa9da516b665ba882a0
Kczmrz <kczmrz@proton.me>
2023-06-05 15:13:35 +0200

    Delete red line

--- src/Components/ToDo.tsx
+++ src/Components/ToDo.tsx
@@ -11,7 +11,7 @@ function ToDo(props:ToDoProps)
     
    return(
    <div className="text-center justify-center tablet:py-2">
-     <div className="hover:text-red-500 hover:line-through"> 
+     <div className="hover:text-red-500"> 
        <h2 className="text-3xl text-white break-all font-Lato todo-anim desktop:text-5xl">{props.activity} &nbsp;
          <button onClick={props.deleteBtn}>
             <FontAwesomeIcon className="text-2xl desktop:text-3xl hover:text-red-500" icon={faTrash} color={'white'}/>
